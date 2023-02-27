f547660f80f4de1e95fd97bd890e3d7f984d698e
Laura Augusta <laurah.algusta@gmail.com>
2023-02-27 19:36:21 -0300

    js function add.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,29 @@
-<script setup>
+<script>
+  export default {
+    data() {
+      return { numberA: '', numberB: '', selected: ''}
+    },
+    computed: {
+    arit() {
+      if (this.selected && this.numberA && this.numberB){
+        if (this.selected === "Adição"){
+          return this.numberA + this.numberB
+        } else if (this.selected === "Subtração"){
+          return this.numberA - this.numberB
+        } else if (this.selected === "Multiplicação"){
+          return this.numberA * this.numberB
+        } else{
+          if (this.numberB === 0){
+            return "Erro matemático!"
+          } else{
+            return this.numberA / this.numberB
+          }
+        }
+      }
+      return "......."
+    },
+  },
+  }
 </script>
 
 <template>
@@ -16,13 +41,13 @@
       <form action="">
         <div class="row">
         <div class="col-md-3">
-          <input class="form-control" type="number" placeholder="Número A" required>
+          <input class="form-control" type="number" placeholder="Número A" required v-model.number="numberA">
         </div>
         <div class="col-md-3">
-          <input class="form-control" type="number" placeholder="Número B" required>
+          <input class="form-control" type="number" placeholder="Número B" required v-model.number="numberB">
         </div>
         <div class="col-md-5">
-          <select class="form-control" id="selection">
+          <select class="form-control" id="selection" v-model="selected">
             <option value='total' selected disabled>Selecionar operação aritmética</option>
             <option>Adição</option>
             <option>Subtração</option>
@@ -41,16 +66,12 @@
 
       <div class="row">
         <div class="col-xs-1 center-block text-center">
-          <span id="result"></span>
+          <span id="result">{{ arit }}</span>
         </div>
       </div>
-      
+
     </div>
   </main>
-  
-  
 </template>
 
-<style scoped>
 
-</style>

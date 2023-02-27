ed7fa2c652fc5b26e40470e2b89150fc506ad735
Laura Augusta <laurah.algusta@gmail.com>
2023-02-27 11:28:42 -0300

    first commi.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container-fluid" id="first">
+      <h1 class="title">Calculadora Aritmética</h1>
+    </div>
+    <div class="side">
+      <img alt="site logo" class="logo" src="./assets/images/calculator-4151004_960_720.png"  />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <div class="container cont-main">
+      <form action="">
+        <div class="row">
+        <div class="col-md-3">
+          <input class="form-control" type="number" placeholder="Número A" required>
+        </div>
+        <div class="col-md-3">
+          <input class="form-control" type="number" placeholder="Número B" required>
+        </div>
+        <div class="col-md-5">
+          <select class="form-control" id="selection">
+            <option value='total' selected disabled>Selecionar operação aritmética</option>
+            <option>Adição</option>
+            <option>Subtração</option>
+            <option>Multiplicação</option>
+            <option>Divisão</option>
+          </select>
+        </div>
+      </div>
+      </form>
+    </div>
+    <div class="container">
+
+      <div class="row">
+        <h4 style="margin-bottom: 16px;">Resultado da operação: </h4>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-1 center-block text-center">
+          <span id="result"></span>
+        </div>
+      </div>
+      
+    </div>
   </main>
+  
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';  // npm i bootstrap@5.3.0-alpha1
 
 import './assets/main.css'
 
